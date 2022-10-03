3dbe930ae3d3effc7cf0ade4678b3312c01d917c
hwrzn <hubert.wrzn@gmail.com>
2022-10-03 17:40:02 +0200

    mvc 1 index.php vers _debut.inc.php

--- index.php
+++ index.php
@@ -1,39 +1,5 @@
 <?php
 
-include "_debut.inc.php";
-
-echo " 
-<br> 
-<table width='80%' cellspacing='0' cellpadding='0' align='center'>
-   <tr>  
-      <td class='texteAccueil'>
-         Cette application web permet de gérer l'hébergement des groupes de sport 
-         durant le festival Sp'or.
-      </td>
-   </tr>
-   <tr>
-      <td>&nbsp;
-      </td>
-   </tr>
-   <tr>
-      <td class='texteAccueil'>
-          Elle offre les services suivants :
-      </td>
-   </tr>
-   <tr>
-      <td>&nbsp;
-      </td>
-   </tr>
-   <tr>
-      <td class='texteAccueil'>
-      <ul>
-         <li>Gérer les établissements (caractéristiques et capacités d'accueil) acceptant d'héberger les participants.
-         <p>
-	      </p>
-         <li>Consulter, réaliser ou modifier les attributions des chambres aux groupes dans les établissements.
-      </ul>
-      </td>
-   </tr>
-</table>";
+include "vue/_debut.inc.php";
 
 ?>
--- vue/_debut.inc.php
+++ vue/_debut.inc.php
@@ -33,7 +33,39 @@ echo '
       Attributions chambres</a></td>
    </tr>
 </table>
-<br>';
+
+<br> 
+<table width="80%" cellspacing="0" cellpadding="0" align="center">
+   <tr>  
+      <td class="texteAccueil">
+         Cette application web permet de gérer l&#39;hébergement des groupes de sport 
+         durant le festival Sp&#39;or.
+      </td>
+   </tr>
+   <tr>
+      <td>&nbsp;
+      </td>
+   </tr>
+   <tr>
+      <td class="texteAccueil">
+          Elle offre les services suivants :
+      </td>
+   </tr>
+   <tr>
+      <td>&nbsp;
+      </td>
+   </tr>
+   <tr>
+      <td class="texteAccueil">
+      <ul>
+         <li>Gérer les établissements (caractéristiques et capacités d&#39;accueil) acceptant d&#39;héberger les participants.
+         <p>
+	      </p>
+         <li>Consulter, réaliser ou modifier les attributions des chambres aux groupes dans les établissements.
+      </ul>
+      </td>
+   </tr>
+</table>';
 
 ?>
 
